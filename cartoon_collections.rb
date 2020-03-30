9baeeb7400bf1f568123ccfd9e4785f3fbb1a0c3
Samuel Lesser <ineedmoreplates@gmail.com>
2020-03-30 03:07:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
 cheese_types = ["cheddar","gouda","camembert"]
 if array & cheese_types
-  result = array & cheese_types.flatten
+  result = "#{array & cheese_types.flatten}"
 else 
   nil
 
