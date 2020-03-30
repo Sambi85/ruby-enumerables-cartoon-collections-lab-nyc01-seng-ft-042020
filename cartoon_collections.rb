f25a0f5c595d674c64a1549d084def741df4135f
Samuel Lesser <ineedmoreplates@gmail.com>
2020-03-30 03:19:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
 cheese_types = ["cheddar","gouda","camembert"]
-pp array.find do |cheese| cheese_types.include?(cheese)
+result = array.find do |cheese| cheese_types.include?(cheese)
 end
-
+pp result
 end
