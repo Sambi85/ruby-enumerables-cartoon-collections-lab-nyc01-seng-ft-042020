961ab03ec1f6863b6febeb56e2809f1f8aee6fc2
Samuel Lesser <ineedmoreplates@gmail.com>
2020-03-30 01:50:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ def find_the_cheese(array)# code an argument here
 cheese_types = ["cheddar","gouda","camembert"]
 
 result = array.find do |food|
-  food == cheese_types? "#{food}" : nil
+  food == cheese_types? "#{food}"
 end
 pp result
 end
